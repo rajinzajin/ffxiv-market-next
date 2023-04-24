d3a772f085590e67a8b65b9cb91760d291462e80
rajinzajin <dorgclabs@gmail.com>
2023-04-24 14:40:21 +0800

    added page title

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,21 +7,25 @@ import Initializer from "@/components/Initializer";
 import Footer from "@/components/Footer";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persiststore } from "@/store/ffxiv_store";
+import Head from "next/head";
 export default function App({ Component, pageProps }) {
 	useEffect(() => {}, []);
 	return (
 		<>
+			<Head>
+				<title>XIVMarket+</title>
+			</Head>
 			<Provider store={store}>
 				<PersistGate loading={null} persistor={persiststore}>
-				<Initializer />
-				<NextNProgress color="#71bfff" />
-				<SideBar />
-				<div className="sm:ml-[25rem] px-6 py-8 min-h-[100vh]">
-					<div className="min-h-[45rem]">
-						<Component {...pageProps} />
+					<Initializer />
+					<NextNProgress color="#71bfff" />
+					<SideBar />
+					<div className="sm:ml-[25rem] px-6 py-8 min-h-[100vh]">
+						<div className="min-h-[45rem]">
+							<Component {...pageProps} />
+						</div>
+						<Footer />
 					</div>
-					<Footer />
-				</div>
 				</PersistGate>
 			</Provider>
 		</>
