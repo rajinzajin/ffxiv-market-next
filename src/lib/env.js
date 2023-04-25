2b11a24fe76a8de4b4316acb6a630b7f3154e30a
rajinzajin <dorgclabs@gmail.com>
2023-04-25 10:42:31 +0800

    testing

---
+++ src/lib/env.js
@@ -0,0 +1,3 @@
+export function getAssetPath() {
+	return process.env.NEXT_PUBLIC_ASSET_PATH;
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,5 @@
+import { getAssetPath } from '@/lib/env'
+import { bg_custom_ffxiv } from '@/styles/bg-custom-ffxiv'
 import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
@@ -7,10 +9,11 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         />
-        <link rel='preload' href='/img/tiny/ffxiv.webp' as='image' />
+        <link rel='preload' href={`${getAssetPath()}img/tiny/ffxiv.webp`} as='image' />
+        <link rel="icon" type="image/png" href={`${getAssetPath()}favicon.ico`}/>
       </Head>
-      <body className='text-white font-body bg-custom-ffxiv'>
-        <div id='globalLoader'>
+      <body className='text-white font-body' style={bg_custom_ffxiv}>
+        <div id='globalLoader' style={bg_custom_ffxiv}>
           <h1 className="font-bold text-xl">LOADING . . .</h1>
         </div>
         <Main />
